7c46df8c0e1b91baa704422fa15b68dc57347d3d
Young Seok Tony Kim <93mighty@gmail.com>
2015-06-14 19:48:44 +0900

    Initial Commit

---
+++ ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Young Seok Kim on 6/14/15.
+//  Copyright (c) 2015 TonyKim. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
